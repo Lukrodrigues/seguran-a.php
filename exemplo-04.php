cb0f14c26cac9ccc6e91b0f948f9a4db7e6582bc
Luciano Rodrigues <luk.rodrigues68@gmail.com>
2018-07-09 01:21:28 -0300

    Colocar pastas seguranca

---
+++ exemplo-04.php
@@ -0,0 +1,10 @@
+<script src='https://www.google.com/recaptcha/api.js'></script>
+<form action="cadastrar.php" method="post">
+	
+	<div class="g-recaptcha" data-sitekey="6Ldy9mIUAAAAANVnYWvVABDsId8Pnw_1kn4Hm85A"></div>
+	<input type="email" name="inputEmail">
+	<button type="submit">Enviar</button>
+
+
+
+</form>
